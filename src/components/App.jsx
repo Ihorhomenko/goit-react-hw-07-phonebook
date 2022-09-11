3a56643cd901b609ba656feaced3f3cf654a84fb
Ihor Khomenko <igorhomenko97@gmail.com>
2022-09-11 14:41:45 +0300

    finished hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { useEffect } from "react";
+import PhoneRegBook from "./PhoneBook/phoneRegBook";
+import Contacts from "./contacts/contacts";
+import Filter from "./filter/filter";
+import { useSelector } from "react-redux";
+
+import { fetchContacts } from "redux/contactsOperations";
+import { useDispatch } from "react-redux";
+
+
 export const App = () => {
+  const normalazeFilter = useSelector(state => state.filter).toLowerCase();
+  const visibleContacts = useSelector(state => state.contacts.items.filter(contact => contact.name.toLowerCase().includes(normalazeFilter)))
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(fetchContacts())
+}, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <div>
+        <section className='phonebook'>
+        <h2>Phonebook</h2>
+        <div className="phone-reg-book">
+          <PhoneRegBook />
+        </div>
+        <div>
+          <h2>Contacts</h2>
+          <Filter onFilter={useSelector(state => state.contacts.filter)} />
+          <Contacts contacts={visibleContacts}/>
+        </div>
+        </section>
+      </div>
   );
 };
+
+// // .filter(contact =>
+// contact.name.toLowerCase().includes(normalazeFilter)
